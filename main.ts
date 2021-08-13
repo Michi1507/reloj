80111c89c69003beefeaca82c8b1bc56edead057
Michi1507 <88722262+Michi1507@users.noreply.github.com>
2021-08-12 20:55:22 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-let segundos = 0
+let segundos = 55
 let minutos = 0
-let horas = 0
+let horas = 3
 basic.forever(function () {
     segundos += 1
     if (segundos > 59) {
         minutos += 1
         segundos = 0
-        basic.showString("1:01")
+        basic.showString("" + horas + "HORAS" + ":" + minutos + "MINUTOS")
         if (minutos > 59) {
             horas += 1
             minutos = 0
-            basic.showString("2:00")
+            basic.showString("" + horas + ":" + minutos)
             if (horas > 23) {
                 horas = 0
             }
